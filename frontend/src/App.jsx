aed8047a1d62c17a380fb1159cdae48b5aa15588
Ville S <ville.suonurmi@helsinki.fi>
2025-05-13 23:32:56 +0300

    front-end perussivu

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+
+
+
+const App = () => {
+
+
+  return (
+    <>
+      <div>
+     <h1>SEB-yhteensopiva finlex-lukija - eli miniflex</h1>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
