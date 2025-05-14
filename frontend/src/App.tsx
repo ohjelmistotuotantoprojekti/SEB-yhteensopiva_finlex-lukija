aa5ef9761d9d1104cf97c93df513c213cabcedc8
Ville S <ville.suonurmi@helsinki.fi>
2025-05-14 14:02:26 +0300

    päivitys

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,50 @@
-import { useState } from 'react'
+interface Law {
+  id: string,
+  name: string
+}
+
+
+import axios from 'axios'
 
 
 const App = () => {
-  const [laws, setLaws] = useState(null)
+
+  const laws: Law[] = [
+    {
+      id: "1",
+      name: "law about averages",
+    },
+    {
+      id: "2",
+      name: "law about bananas",
+    },
+    {
+      id: "3",
+      name: "law about something",
+    },
+  ]
+
+
+
+ 
+
+  /**  Hakee backendiltä dataa
+  const getJson = async () => {
+    const url: string = 'http://localhost:3001/notes'
+    await axios.get(url).then(response => {
+        console.log(response.data)
+    })
+  }
+  */
+
 
   return (
-    <>
-    Heippa
-    </>
+    <div>
+    <h3>Tässä on lait:</h3>
+    <ul>
+    {laws.map((law) =><li key={law.id}>{law.name}</li>) }
+    </ul>
+    </div>
   )
 }
 
