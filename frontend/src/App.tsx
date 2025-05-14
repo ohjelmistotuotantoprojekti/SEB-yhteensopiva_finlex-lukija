6c644822869a6b742aa456e36fc655b4532c5ff2
Ville S <ville.suonurmi@helsinki.fi>
2025-05-14 22:00:49 +0300

    App.tsx tehty modulit haulle ja listaukselle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,32 +1,92 @@
 interface Law {
-  id: string,
-  name: string
+  docYear:  string,
+  docNumber: number,
+  docTitle: string
+}
+
+interface Props {
+  search: string,
+  handleSearchInputChange: (event: React.SyntheticEvent) => void,
+  handleSearchEvent: (event: React.SyntheticEvent) => void
+
 }
 
 
 import axios from 'axios'
+import { useState } from 'react'
+
+
+
+const SearchForm = ({search, handleSearchInputChange, handleSearchEvent} : Props ) => {
+
+  return (
+       <div>
+        <form id="searchForm" onSubmit={handleSearchEvent}>
+          <input type="text" id="searchField" 
+         onChange={handleSearchInputChange}
+          value={search}
+          placeholder="Search"
+          />
+        </form>
+      </div>
+   )
+}
+
+
+
+const LawList = ({laws}: {laws: Law[]}) => {
+
+  const listStyle = {
+    width: "500px",
+    backgroundColor: "#eeeeee",
+    padding: '10px',
+    margin: '4px',
+  }
+
+  return (
+    <>
+   
+    { laws.map((law) =><div style={listStyle} key={law.docNumber}>{law.docYear}/{law.docNumber} {law.docTitle}</div>) }
+  
+    </>
+  )
+
+}
 
 
 const App = () => {
 
   const laws: Law[] = [
     {
-      id: "1",
-      name: "law about averages",
+      docYear: "2024",
+      docNumber: 244,
+      docTitle: "law about averages",
     },
     {
-      id: "2",
-      name: "law about bananas",
+      docYear: "2024",
+      docNumber: 245,
+      docTitle: "law about bananas",
     },
     {
-      id: "3",
-      name: "law about something",
+      docYear: "2024",
+      docNumber: 246,
+      docTitle: "law about something",
     },
+   
   ]
 
+  const [search, setSearch] = useState<string>('')
 
 
- 
+  const handleSearchEvent = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    console.log(search)
+  
+  }
+  const handleSearchInputChange = (event: React.SyntheticEvent) => {
+    setSearch(event.target.value)
+  }
+
 
   /**  Hakee backendiltä dataa
   const getJson = async () => {
@@ -38,12 +98,19 @@ const App = () => {
   */
 
 
+
+
   return (
     <div>
-    <h3>Tässä on lait:</h3>
-    <ul>
-    {laws.map((law) =><li key={law.id}>{law.name}</li>) }
-    </ul>
+    <h3>Lakitekstit:</h3>
+    
+   
+    <SearchForm search={search}  
+                handleSearchInputChange={handleSearchInputChange}
+                handleSearchEvent={handleSearchEvent} 
+    />
+
+    <LawList laws={laws} />
     </div>
   )
 }
