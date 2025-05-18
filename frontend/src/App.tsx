cf0f9675280b04d37b9d22578db852cbdd4deffa
Ville Salmela <villesalmela@users.noreply.github.com>
2025-05-18 16:07:55 +0300

    siirry käyttämään suhteellisia polkuja

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,25 +8,12 @@ import LawPage from './components/LawPage'
 
 const App = () => {
 
-  let port: string =  ''
-  let url: string =  ''
-  let server: string = ''
-
-  if (import.meta.env.DEV) {
-    port = '3001'
-    url = 'http://localhost'
-  }
-  
-  if (url != "") {
-    server = `${url}:${port}` 
-  }
-
   return (
     <Router>
     <div>
     <Routes>
-      <Route key="listpage" path="/" element={<ListPage server={server} />} />
-      <Route key="lawpage" path="/lainsaadanto/:year/:id" element={<LawPage server={server} />} />
+      <Route key="listpage" path="/" element={<ListPage/>} />
+      <Route key="lawpage" path="/lainsaadanto/:year/:id" element={<LawPage/>} />
     </Routes>
     </div>
     </Router>
