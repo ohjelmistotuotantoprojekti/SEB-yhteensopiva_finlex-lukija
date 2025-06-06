9197d803039e8b4ed5613134fd372163e379dd54
Ville Salmela <villesalmela@users.noreply.github.com>
2025-06-06 19:23:27 +0300

    palauta tulosten muotoilu edeltävään tilaan

--- frontend/src/components/DocumentList.tsx
+++ frontend/src/components/DocumentList.tsx
@@ -1,4 +1,4 @@
-import type {DocumentListProps} from "../types"
+import type {DocumentListProps, Document} from "../types"
 import {nanoid} from 'nanoid'
 
 
@@ -11,13 +11,32 @@ const DocumentList = ({laws, frontsection}: DocumentListProps) => {
     padding: '10px',
     margin: '4px',
   }
+
+  function prepareLink(doc: Document): string {
+    return `/${frontsection}/${doc.docYear}/${doc.docNumber}${doc.docLevel ? `/${doc.docLevel}` : ""}`;
+  }
+
+  function prepareLabel(doc: Document): string {
+    if (doc.docLevel) {
+      return `${doc.docLevel.toUpperCase()}:${doc.docYear}:${doc.docNumber}`;
+    }
+    else {
+      return `${doc.docNumber}/${doc.docYear}`;
+    }
+  }
+
+  function prepareKey(doc: Document): string {
+    return `${doc.docLevel ? doc.docLevel : ""}${doc.docYear}${doc.docNumber}${nanoid(2)}`;
+  }
   
   return (
     <>
     { laws.map((law) => 
-        <div style={listStyle} key={`${law.docLevel}${law.docYear}${law.docNumber}${nanoid(2)}`} >
-        <a href={`/${frontsection}/${law.docYear}/${law.docNumber}/${law.docLevel ? law.docLevel : ""}`}>
-        {law.docLevel ? law.docLevel.toUpperCase(): ""}{law.docTitle ? law.docTitle.toUpperCase(): ""}:{law.docYear}:{law.docNumber}</a></div>
+        <div style={listStyle} key={prepareKey(law)} >
+          <a href={prepareLink(law)}>
+            {prepareLabel(law)} {law.docTitle ? law.docTitle : ""}
+          </a>
+        </div>
       )
     }
     </>
