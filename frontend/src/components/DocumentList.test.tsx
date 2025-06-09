24e124580267329171a8f378255196c08bffd725
Ville S <ville.suonurmi@helsinki.fi>
2025-06-09 13:14:25 +0300

    frontin tunailua - testit fixat

--- frontend/src/components/DocumentList.test.tsx
+++ frontend/src/components/DocumentList.test.tsx
@@ -18,9 +18,9 @@ test('renders content', () => {
     },]
    
     render(<DocumentList laws={laws} frontsection={"statute"} />)
-    const element = screen.getByText('123/2000 Title')
+    const element = screen.getByText('123/2000 - Title')
     expect(element).toBeDefined()
-    const element2 = screen.getByText('321/2000 Title2')
+    const element2 = screen.getByText('321/2000 - Title2')
     expect(element2).toBeDefined()
     
 
