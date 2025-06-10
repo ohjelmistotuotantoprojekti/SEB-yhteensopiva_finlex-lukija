75c3398374607a334137aa58469b13fc3450b71a
Lauri Huldin <127834326+LHuldin@users.noreply.github.com>
2025-06-10 14:30:47 +0300

    pikku fixeja

--- backend/src/startdev.ts
+++ backend/src/startdev.ts
@@ -1,6 +1,9 @@
 //const { spawn } = require('child_process');
 import { spawn } from 'child_process';
 
-spawn('npx', ['tsx', 'src', '/', 'dbSetup.js'], { stdio: 'inherit' });
-spawn('npx', ['tsx', 'src', '/', 'index.js'], { stdio: 'inherit' });
+//spawn('npx', ['tsx', 'src', '/', 'dbSetup.js'], { stdio: 'inherit' });
+//spawn('npx', ['tsx', 'src', '/', 'index.js'], { stdio: 'inherit' });
+
+spawn('npx', ['tsx', 'src/dbSetup.ts'], { stdio: 'inherit' });
+spawn('npx', ['tsx', 'src/index.ts'], { stdio: 'inherit' });
 
