61c1bf7d69a753479172914e4d94c5060d10077e
Eva-Maria <evamaria.asplund@gmail.com>
2025-06-11 11:12:21 +0300

    test 400

--- backend/e2etest/finlex_app.spec.js
+++ backend/e2etest/finlex_app.spec.js
@@ -2,6 +2,8 @@ import playwright from '@playwright/test'
 import { PostgreSqlContainer } from '@testcontainers/postgresql';
 import { setPool, createTables, closePool } from '../src/db/db.js'
 import { setSingleJudgment, setSingleStatute } from '../src/db/load.js'
+/*
+import { afterEach } from 'node:test';
 
 let container;
 let databaseUrl;
@@ -20,12 +22,12 @@ playwright.describe("Main page", () => {
     await setSingleJudgment("https://www.finlex.fi/fi/oikeuskaytanto/korkein-oikeus/ennakkopaatokset/1975/II-16")
     await page.goto('http://localhost:3001')
   })
-  /*
-  afterAll(async ({ page }) => {
+
+  playwright.afterEach(async ({ page }) => {
     await closePool();
     await container.stop();
   })
-*/
+
   playwright.test('front page can be opened', async ({ page }) => {
     const locator = await page.getByText("Lainsäädäntö:")
     await playwright.expect(locator).toBeVisible()
@@ -33,8 +35,8 @@ playwright.describe("Main page", () => {
     await playwright.expect(page.getByRole("button", {name: "Hae"})).toBeVisible()
     await playwright.expect(page.getByRole("combobox")).toBeVisible()
   })
-/*
-  test('language can be changed to swedish', async ({ page }) => {
+
+  playwright.test('language can be changed to swedish', async ({ page }) => {
     await page.getByRole("combobox").selectOption("Ruotsi")
 
     const locator = await page.getByText("Lagstiftning")
@@ -44,19 +46,19 @@ playwright.describe("Main page", () => {
     await playwright.expect(page.getByRole("combobox")).toBeVisible()
   })
 
-  test('searchbar can be used to search (fin)', async ({ page }) => {
+  playwright.test('searchbar can be used to search (fin)', async ({ page }) => {
     await page.getByRole("textbox").fill("laki")
     await page.getByRole("button", {name: "Hae"}).click()
     await playwright.expect(page.getByText("/").first()).toBeVisible()
   })
 
-  test('searchbar shows error message (fin)', async ({ page }) => {
+  playwright.test('searchbar shows error message (fin)', async ({ page }) => {
     await page.getByRole("textbox").fill("")
     await page.getByRole("button", {name: "Hae"}).click()
     await playwright.expect(page.getByText("Haulla ei löytynyt hakutuloksia")).toBeVisible()
   })
 
-  test('searchbar can be used to search (swe)', async ({ page }) => {
+  playwright.test('searchbar can be used to search (swe)', async ({ page }) => {
     await page.getByRole("combobox").selectOption("Ruotsi")
 
     await page.getByRole("textbox").fill("lag")
@@ -64,21 +66,21 @@ playwright.describe("Main page", () => {
     await playwright.expect(page.getByText("/").first()).toBeVisible()
   })
 
-  test('searchbar shows error message (swe)', async ({ page }) => {
+  playwright.test('searchbar shows error message (swe)', async ({ page }) => {
     await page.getByRole("combobox").selectOption("Ruotsi")
     await page.getByRole("textbox").fill("")
     await page.getByRole("button", {name: "Sök"}).click()
     await playwright.expect(page.getByText("Inga sökresultat")).toBeVisible()
   })
 
-  test('searchresult can be opened', async ({ page }) => {
+  playwright.test('searchresult can be opened', async ({ page }) => {
     await page.getByRole("textbox").fill("luonnonsuojelulaki")
     await page.getByRole("button", {name: "Hae"}).click()
     await page.getByText("Luonnonsuojelulaki").click()
     await playwright.expect(page.getByText("Metadata")).toBeVisible()
   })
 
-  test('single law page can be opened (fin)', async ({ page }) => {
+  playwright.test('single law page can be opened (fin)', async ({ page }) => {
     await page.getByRole("textbox").fill("9/2023")
     await page.getByRole("button", {name: "Hae"}).click()
     const locator = await page.getByText("9/2023 – Luonnonsuojelulaki")
@@ -87,7 +89,7 @@ playwright.describe("Main page", () => {
     await playwright.expect(page.getByRole('link', { name: 'Takaisin', exact: true})).toBeVisible()
   })
 
-  test('single law page can be opened (swe)', async ({ page }) => {
+  playwright.test('single law page can be opened (swe)', async ({ page }) => {
     await page.getByRole("combobox").selectOption("Ruotsi")
     await page.getByRole("textbox").fill("9/2023")
     await page.getByRole("button", {name: "Sök"}).click()
@@ -96,20 +98,20 @@ playwright.describe("Main page", () => {
     await playwright.expect(page.getByText("Metadata")).toBeVisible()
     await playwright.expect(page.getByRole('link', { name: 'Tillbaka', exact: true})).toBeVisible()
   })
-*/
+
 })
-/*
-describe("Single law page", () => {
-  beforeAll(async ({ page }) => {
+*/
+playwright.describe("Single law page", () => {
+  playwright.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3001/lainsaadanto/2023/9')
   })
 
-  afterAll(async ({ page }) => {
+  playwright.afterEach(async ({ page }) => {
     await closePool();
     await container.stop();
   })
 
-test('back button brings back to main page', async ({ page }) => {
+playwright.test('back button brings back to main page', async ({ page }) => {
     const locator1 = await page.getByText("9/2023 – Luonnonsuojelulaki")
     await playwright.expect(locator1).toBeVisible()
     await page.getByRole('link', { name: 'Takaisin', exact: true}).click()
@@ -120,59 +122,59 @@ test('back button brings back to main page', async ({ page }) => {
     await playwright.expect(page.getByRole("combobox")).toBeVisible()
   })
 
-test('language can be changed to swedish', async ({ page }) => {
+playwright.test('language can be changed to swedish', async ({ page }) => {
     pass
   })
 
-test('language stays after back button is pressed', async ({ page }) => {
+playwright.test('language stays after back button is pressed', async ({ page }) => {
     pass
   })
 
 })
 
-describe("Case law page", () => {
-  beforeAll(async ({ page }) => {
+playwright.describe("Case law page", () => {
+  playwright.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3001/oikeuskaytanto')
   })
 
-  afterAll(async ({ page }) => {
+  playwright.afterEach(async ({ page }) => {
     await closePool();
     await container.stop();
   })
 
-test('page can be opened', async ({ page }) => {
+playwright.test('page can be opened', async ({ page }) => {
     pass
   })
 
-test('language can be changed to swedish', async ({ page }) => {
+playwright.test('language can be changed to swedish', async ({ page }) => {
     pass
   })
 
-test('searchbar gives error (fin)', async ({ page }) => {
+playwright.test('searchbar gives error (fin)', async ({ page }) => {
     pass
   })
 
-test('searchbar gives error (swe)', async ({ page }) => {
+playwright.test('searchbar gives error (swe)', async ({ page }) => {
     pass
   })
 
-test('searchbar can be used to search (fin)', async ({ page }) => {
+playwright.test('searchbar can be used to search (fin)', async ({ page }) => {
     pass
   })
 
-test('searchbar can be used to search (swe)', async ({ page }) => {
+playwright.test('searchbar can be used to search (swe)', async ({ page }) => {
     pass
   })
 
-test('single case can be searched (fin)', async ({ page }) => {
+playwright.test('single case can be searched (fin)', async ({ page }) => {
     pass
   })
 
-test('single case can be searched (swe)', async ({ page }) => {
+playwright.test('single case can be searched (swe)', async ({ page }) => {
     pass
   })
 
-test('single case law can be opened', async ({ page }) => {
+playwright.test('single case law can be opened', async ({ page }) => {
     pass
   })
 
@@ -180,27 +182,26 @@ test('single case law can be opened', async ({ page }) => {
 
 })
 
-describe("Single case law page", () => {
-  beforeAll(async ({ page }) => {
+playwright.describe("Single case law page", () => {
+  playwright.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:3001/oikeuskaytanto/2005/13/kho')
   })
 
-  afterAll(async ({ page }) => {
+  playwright.afterEach(async ({ page }) => {
     await closePool();
     await container.stop();
   })
 
-test('back button brings back to case law page', async ({ page }) => {
+playwright.test('back button brings back to case law page', async ({ page }) => {
     pass
   })
 
-test('language can be changed to swedish', async ({ page }) => {
+playwright.test('language can be changed to swedish', async ({ page }) => {
     pass
   })
 
-test('language stays after back button is pressed', async ({ page }) => {
+playwright.test('language stays after back button is pressed', async ({ page }) => {
     pass
   })
 
 })
-*/
