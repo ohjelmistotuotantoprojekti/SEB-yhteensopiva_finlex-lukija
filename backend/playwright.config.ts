c1bc5fafde3dc8bbe72a1e607d4ee353f808d842
Ville S <ville.suonurmi@helsinki.fi>
2025-06-16 14:30:28 +0300

    front fixes

--- backend/playwright.config.ts
+++ backend/playwright.config.ts
@@ -14,8 +14,8 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './e2etest',
   /* Run tests in files in parallel */
-  timeout: 500,
-  expect: { timeout: 100 },
+  timeout: 1000,
+  expect: { timeout: 1000 },
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
